ecd75f473543110451270a888f05eb23bd793ff7
Yohsuke Fukai <ysk@yfukai.net>
2026-01-26 14:56:11 +0900

    moved files

--- modules/correct_shading/resources/usr/bin/shading_correction.py
+++ modules/correct_shading/resources/usr/bin/shading_correction.py
@@ -9,20 +9,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 from utils import read_mosaic_image
 
+STRATEGIES = ["percentile", "mean", "pybaselines"]
+PYBASELINES_METHODS = ["imodpoly"]
+
 @dataclass
 class Config:
     file_path : str = "testdata/test.czi"
     metadata_path : str = "testdata/metadata.yaml"
     channel_index : int = 0
     strategy : str = "percentile" # options: "percentile", "mean", "pybaselines"
+    local_subtraction : bool = False
     percentile_percentile : float = 50.0
-    pybaselines_method : str = 
+    pybaselines_method : str = "imodpoly" # options: "imodpoly"
     output_path : str = "testdata/test_output"
     output_correction_data_filename_prefix : str = "shading_correction"
     output_test_image_filename_prefix : str = "background"
 
 def main():
     cfg = pyrallis.parse(config_class=Config)
+    if cfg.strategy not in STRATEGIES:
+        raise ValueError(f"Invalid strategy: {cfg.strategy}. Must be one of {STRATEGIES}")
+    if cfg.strategy == "pybaselines" and cfg.pybaselines_method not in PYBASELINES_METHODS:
+        raise ValueError(f"Invalid pybaselines method: {cfg.pybaselines_method}. Must be one of {PYBASELINES_METHODS}")
+    
     print(f"File Path: {cfg.file_path}")
     print(f"Metadata Path: {cfg.metadata_path}")
 
@@ -36,7 +45,6 @@ def main():
 
     print(f"Metadata: {metadata}")
     print(f"Image data shape: {image_data.shape}")
-
 
 if __name__ == '__main__':
     main()
